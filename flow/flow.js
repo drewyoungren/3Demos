4f39845bbbf83de40c037e102578a6b55c98c36d
Drew Youngren <dcy2@columbia.edu>
2020-11-29 18:51:41 -0500

    Animate by time

--- flow/flow.js
+++ flow/flow.js
@@ -4,7 +4,7 @@ import * as THREE from 'https://unpkg.com/three@0.121.0/build/three.module.js';
 import {OrbitControls} from 'https://unpkg.com/three@0.121.0/examples/jsm/controls/OrbitControls.js';
 // import {Lut} from 'https://unpkg.com/three@0.121.0/examples/jsm/math/Lut.js';
 import { GUI} from '../base/dat.gui.module.js';
-import { colorBufferVertices, blueUpRedDown, addColorBar, ArrowBufferGeometry } from "../base/utils.js";
+import  { ArrowBufferGeometry, drawAxes, drawGrid, labelAxes }  from "../base/utils.js";
 
 // Make z the default up
 THREE.Object3D.DefaultUp.set(0,0,1);
@@ -83,232 +83,48 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // controls.keys = [ 65, 83, 68 ];
 
 controls.target.set( 0,0,0);
-controls.addEventListener('change',render);
+// controls.addEventListener('change',render);
 
-window.addEventListener('resize', render);
+// window.addEventListener('resize', render);
 
-//axes
+// Grid
 
-const lineMaterial = new THREE.LineBasicMaterial( { color: 0x551122, transparent: true, opacity: 0.8 } );
-
-let gridMeshes = new THREE.Object3D();
-function drawGrid(coords='rect') {
-  let points = [];
-  for (let index = gridMeshes.children.length-1; index >= 0; index++) {
-    const element = gridMeshes.children[index];
-    element.geometry.dispose();
-    gridMeshes.remove(element);
-  }
-  let geometry;
-  if (coords === 'rect'){
-    for (let j = -(10*gridMax); j <= (10*gridMax); j += gridStep) {
-        points.push( new THREE.Vector3( j, -(10*gridMax), 0 ) );
-        points.push( new THREE.Vector3( j, (10*gridMax), 0 ) );
-
-        // let geometry = new THREE.BufferGeometry().setFromPoints( points );
-        // gridMeshes.add(new THREE.Line(geometry,lineMaterial));
-        
-        // points = [];
-
-        points.push( new THREE.Vector3( -(10*gridMax), j , 0) );
-        points.push( new THREE.Vector3( (10*gridMax), j , 0) );
-
-      }
-    } else {
-      for (let i = 0; i <= 10*gridMax; i += gridStep) {
-        for (let j = 0; j < 100; j++) {
-          points.push( new THREE.Vector3( i*Math.cos(2*pi*j/100), i*Math.sin(2*pi*j/100) , 0) );
-          points.push( new THREE.Vector3( i*Math.cos(2*pi*(j + 1)/100), i*Math.sin(2*pi*(j + 1)/100) , 0) );
-        }
-      }
-      for (let i = 0; i < 16 ; i++) {
-        points.push( new THREE.Vector3( 10*gridMax*Math.cos(pi*i/8), 10*gridMax*Math.sin(pi*i/8) , 0) );
-        points.push( new THREE.Vector3( 0, 0, 0 ) );
-      }
-    }
-    geometry = new THREE.BufferGeometry().setFromPoints( points );
-    gridMeshes.add(new THREE.LineSegments(geometry,lineMaterial));
-}
+let gridMeshes = drawGrid();
 
 scene.add(gridMeshes);
-drawGrid();
-// drawGrid('polar');
 
 // Axes
-const axesHolder = new THREE.Object3D();
-scene.add(axesHolder);
 const axesMaterial = new THREE.MeshLambertMaterial( {color: 0x320032} );
-for (let index = 0; index < 3; index++) {
-  let geometry = new THREE.CylinderGeometry( gridStep/16, gridStep/16, gridMax*3, 12 );
-  let coneGeometry = new THREE.ConeGeometry( gridStep/10, gridStep/3, 12 );
-  let cylinder = new THREE.Mesh( geometry, axesMaterial );
-  // cylinder.position.y = gridMax*1.1/2;
-  let cone = new THREE.Mesh(coneGeometry,axesMaterial);
-  if (index === 0) {
-    cylinder.rotation.x = Math.PI/2;
-  } else { if (index === 2) {
-    cylinder.rotation.z = -Math.PI/2;
-  }}
-  cone.position.y = gridMax*3/2 + gridStep/6;
-  cylinder.add(cone);
-  axesHolder.add( cylinder );
-}
+const axesHolder = drawAxes( {gridMax, gridStep, axesMaterial});
+scene.add(axesHolder)
 
 
-// Fonts
-var axesText = [];
-var loader = new THREE.FontLoader();
-var font = loader.load(
-	// resource URL
-	'../fonts/P052_Italic.json',
-  function (font) {
-    var xyz = ['x','y','z'];
-    var tPos = 1.7*gridMax;
-    for (var i = 0; i < 3; i++) {
-      var textGeo = new THREE.TextGeometry( xyz[i], {
-      font: font,
-      size: gridStep,
-      height: 0,
-      curveSegments: 12,
-      bevelEnabled: false
-    } );
-      // var textMaterial = new THREE.MeshBasicMaterial({color: axesMaterial})
-      var textHolder = new THREE.Object3D();
-      var text = new THREE.Mesh(textGeo,axesMaterial);
-      // text.computeBoundingBox();
-      
-      textGeo.computeBoundingBox();
-      textGeo.boundingBox.getCenter(text.position).multiplyScalar(-1);
+const [axesText, font] = labelAxes( { scene } );
 
-      if (i === 0) { textHolder.position.x = tPos; } else {
-        if (i === 1) {
-          textHolder.position.y = tPos;
-        } else { textHolder.position.z = tPos; }
-      }
-      scene.add(textHolder);
-      textHolder.add(text);
-
-      axesText.push(textHolder);
-      // console.log("pushed: ",'xyz'[i])
-      if (render) {render();}
-    }
-  },
-  // onProgress callback
-	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	},
-
-	// onError callback
-	function ( err ) {
-		console.log( 'An error happened' );
-	}
-);
-
-function disposeArray() {
+const disposeArray = () => {
   this.array = null;
 }
 
-
 let gui = new GUI();
 
-
-
 const material = new THREE.MeshPhongMaterial({color: 0xdd22ff,shininess: 60,side: THREE.DoubleSide,vertexColors: false});
-const materialRandom = new THREE.MeshPhongMaterial({color: 0x0000b4,shininess: 70,side: THREE.FrontSide,vertexColors: false});
-const whiteLineMaterial = new THREE.LineBasicMaterial({color: 0xffffff,linewidth: 2});
-const redLineMaterial = new THREE.LineBasicMaterial({color: 0xbb0000,linewidth: 14});
+// const materialRandom = new THREE.MeshPhongMaterial({color: 0x0000b4,shininess: 70,side: THREE.FrontSide,vertexColors: false});
+// const whiteLineMaterial = new THREE.LineBasicMaterial({color: 0xffffff,linewidth: 2});
+// const redLineMaterial = new THREE.LineBasicMaterial({color: 0xbb0000,linewidth: 14});
 
 
-const wireMaterial = new THREE.MeshBasicMaterial( { color: 0x333333, wireframe: true } );
-const minusMaterial = new THREE.MeshPhongMaterial({color: 0xff3232, shininess: 80, side: THREE.BackSide,vertexColors: false, transparent: true, opacity: 0.4});
-const plusMaterial = new THREE.MeshPhongMaterial({color: 0x3232ff, shininess: 80, side: THREE.FrontSide,vertexColors: false, transparent: true, opacity: 0.4});
+// const wireMaterial = new THREE.MeshBasicMaterial( { color: 0x333333, wireframe: true } );
+// const minusMaterial = new THREE.MeshPhongMaterial({color: 0xff3232, shininess: 80, side: THREE.BackSide, vertexColors: false, transparent: true, opacity: 0.4});
+// const plusMaterial = new THREE.MeshPhongMaterial({color: 0x3232ff, shininess: 80, side: THREE.FrontSide, vertexColors: false, transparent: true, opacity: 0.4});
 
 
 
-class ParametricCurve extends THREE.Curve {
-
-	constructor( scale = 1, r = (t) => new THREE.Vector3(t,t,t), a = 0, b = 1 ) {
-
-		super();
-
-    this.scale = scale;
-    
-    this.r = r;
-
-    this.a = a;
-
-    this.b = b;
-
-	}
-
-	getPoint( t, optionalTarget = new THREE.Vector3() ) {
-
-    const s = this.a + (this.b - this.a)*t;
-
-    const {x,y,z} = this.r(s);
-
-		return optionalTarget.set( x,y,z ).multiplyScalar( this.scale );
-
-	}
-
-}
 
 
-const r1 = ((t) => new THREE.Vector3(t,t**2,t**3));
-const r2 = ((t) => new THREE.Vector3((1+t/4)*Math.cos(4*pi*t),2*t - Math.sin(8*t)/2,0));
-
-
-const curves = {
-  parabola: {
-    func: (t) => new THREE.Vector3(t,t*t - 1,0),
-    a: -1,
-    b: 2,
-    tex: {
-      x: "t",
-      y: "t^2 -1",
-      xPrime: "1",
-      yPrime: "2t"
-    }
-  },
-  swirl: {
-    func: (t) => new THREE.Vector3((1+t/4)*Math.cos(4*pi*t),2*t - Math.sin(8*t)/2,0),
-    a: 0,
-    b: 1,
-    tex: {
-      x: "(1+t/4)\\cos(4 \\pi t)",
-      y: "2t - \\sin(8t)/2",
-      xPrime: "\\cos(4 \\pi t)/4 - (1+t/4)\\sin(4 \\pi t)4\\pi",
-      yPrime: "2 - 4\\sin(8t)"
-    }
-  },
-  line: {
-    func: (t) => new THREE.Vector3(-1+ 2*t,-1 + 3*t,0),
-    a: 0,
-    b: 1,
-    tex: {
-      x: "2t - 1",
-      y: "3t -1",
-      xPrime: "2",
-      yPrime: "3"
-    }
-  },
-  circle: {
-    func: (t) => new THREE.Vector3(Math.cos(t),Math.sin(t),0),
-    a: 0,
-    b: 2*Math.PI,
-    tex: {
-      x: "\\cos t",
-      y: "\\sin t",
-      xPrime: "-\\sin t",
-      yPrime: "\\cos t",
-      b: "2\\pi",
-    }
-  },
-}
 
 const fields = {
   'wave': {
-    func: (x,y,z,vec) => {vec.set(1,Math.sin(2*x)/2,-Math.sin(z)); return vec;},
+    func: (x,y,z,vec) => {vec.set(1, Math.sin(2 * x) / 2, -Math.sin(4 * z) / 2); return vec;},
     tex: " \,\vec i + \sin(x) \,\vec j + \sin(z) \,\vec k",
   },
   'constant': {
@@ -387,11 +203,7 @@ const data = {
 //   }
 // }`
 
-gui.add(data,'field',Object.keys(fields)).onChange(() => {
-  // updateField();
-  counter = 0;
-  animate();
-})
+gui.add(data,'field',Object.keys(fields))
 
 // updateField(1,0.5);
 
@@ -401,7 +213,7 @@ const arrowGeometries = [];
 for (let i = 1; i < 51; i++) {
   arrowGeometries.push(new ArrowBufferGeometry( Math.min(1/30,i/100), Math.min(1/60,i/100), i/100, Math.min(1/10, i/100)));
 }
-const arrowGeometry = new ArrowBufferGeometry( 1/20, 1/50, 1/2, 1/12 );
+
 
 for (let i = 0; i < 8; i++) {
   for (let j = 0; j < 8; j++) {
@@ -417,7 +229,7 @@ for (let i = 0; i < 8; i++) {
     
     arrow.position.set( pos.x, pos.y, pos.z );
     vec = fields[data.field].func( pos.x, pos.y, pos.z, vec );
-    console.log( "setup", i, j, pos, vec)
+    // console.log( "setup", i, j, pos, vec)
     arrow.lookAt(pos.x + vec.x, pos.y + vec.y, pos.z + vec.z);
     // arrow.add(arrowMesh);
     quiver.add( arrow );
@@ -425,10 +237,17 @@ for (let i = 0; i < 8; i++) {
   }
 }
 
-let counter = 0;
+let counter = 0, last;
 
-function animate() {
-  const dt = 1/60;
+
+function animate( time ) {
+  let dt;
+  if (last === undefined) {
+    dt = 0;
+  } else {
+    dt = (time - last) * 0.001;
+  }
+  last = time;
   const F = fields[data.field].func;
   let vec = new THREE.Vector3();
 
@@ -471,7 +290,8 @@ function resizeRendererToDisplaySize(renderer) {
   }
 
 function render() {
-    // controls.update();
+    controls.update();
+
     for (let index = 0; index < axesText.length; index++) {
       const element = axesText[index];
       element.lookAt(camera.position);
